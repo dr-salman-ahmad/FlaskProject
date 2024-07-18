68161d1ce39c3dfc45e1d00946f372fb083408ab
Salman Ahmad <ssalmanmughal73@gmail.com>
2024-07-18 16:33:14 +0500

    Class Based Views

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,18 +1,19 @@
 import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from flask.views import MethodView
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
+class RegisterView(MethodView):
+    def get(self):
+        return render_template('auth/register.html')
+
+    def post(self):
         username = request.form['username']
         password = request.form['password']
         db = get_db()
@@ -36,13 +37,14 @@ def register():
                 return redirect(url_for("auth.login"))
 
         flash(error)
+        return render_template('auth/register.html')
 
-    return render_template('auth/register.html')
 
+class LoginView(MethodView):
+    def get(self):
+        return render_template('auth/login.html')
 
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
+    def post(self):
         username = request.form['username']
         password = request.form['password']
         db = get_db()
@@ -62,8 +64,13 @@ def login():
             return redirect(url_for('index'))
 
         flash(error)
+        return render_template('auth/login.html')
 
-    return render_template('auth/login.html')
+
+class LogoutView(MethodView):
+    def get(self):
+        session.clear()
+        return redirect(url_for('index'))
 
 
 @bp.before_app_request
@@ -78,18 +85,16 @@ def load_logged_in_user():
         ).fetchone()
 
 
-@bp.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
-
-
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
             return redirect(url_for('auth.login'))
-
         return view(**kwargs)
 
     return wrapped_view
+
+
+bp.add_url_rule('/register', view_func=RegisterView.as_view('register'))
+bp.add_url_rule('/login', view_func=LoginView.as_view('login'))
+bp.add_url_rule('/logout', view_func=LogoutView.as_view('logout'))
